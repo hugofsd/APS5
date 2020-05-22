7a1186087548e7afbe3341e7c36b011801e22639
hugofsd <hugo619@outlook.com>
2020-05-22 03:36:38 -0300

    Tela finalizada, aguardando recebimento do conteudo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MenuItem, MessageService, ConfirmationService } from 'primeng/api';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { style } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
 export class AppComponent {
   title = 'apsunip';
+  items: MenuItem[];
+  home: MenuItem;
+  graficoone: any;
+  msgs: Message[] = [];
+  position: string;
+  display: boolean = false;
+  displaytwo: boolean = false;
+  graficotwo: any;
+  cars: any[];
+  constructor(
+  ) {
+
+    this.graficoone = {
+        labels: ['Segunda-Feira', 'Terça-Feira', 'Quarta-Feira',
+         'Quinta-Feira', 'Sexta-Feira', 'Sabado', 'Domingo'],
+        datasets: [
+            {
+                label: 'Semana atual',
+                backgroundColor: '#42A5F5',
+                borderColor: '#1E88E5',
+                data: [90, 49, 40, 35, 70, 100, 65]
+            },
+
+            {
+              label: 'Semana passada',
+              backgroundColor: '#9CCC65',
+              borderColor: '#1E88E5',
+              data: [34, 33, 22, 40, 50, 41, 30]
+          },
+
+        ]
+    }
+
+    this.graficotwo = {
+      labels: ['Norte', 'LESTE', 'SUL', 'OESTE'],
+      datasets: [
+          {
+              label: 'Semena passada',
+              backgroundColor: 'rgba(179,181,198,0.2)',
+              borderColor: 'rgba(179,181,198,1)',
+              pointBackgroundColor: 'rgba(179,181,198,1)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(179,181,198,0)',
+              data: [65, 59, 90, 81]
+          },
+          {
+              label: 'Semana atual',
+              backgroundColor: 'rgba(255,99,132,0.2)',
+              borderColor: 'rgba(255,99,132,1)',
+              pointBackgroundColor: 'rgba(255,99,132,0)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(255,99,132,1)',
+              data: [28, 48, 40, 19]
+          }
+      ]
+  };
+}
+
+ngOnInit() {
+   this.items = [
+            {label: 'Link para o GitHub', icon: 'pi pi-fw pi-file', url: 'https://github.com/hugofsd/APS5'}
+          ];
+    }
+
+
+informacaoGrafico(){
+ this.display = true;
+}
+
+informacaoGraficotwo(){
+  this.displaytwo = true;
+ }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
+import {ChartModule} from 'primeng/chart';
+
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+import {MegaMenuItem} from 'primeng/api';  //required when using MegaMenu
+import {ToolbarModule} from 'primeng/toolbar';
+import {AccordionModule} from 'primeng/accordion';
+import {CarouselModule} from 'primeng/carousel';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+import {DialogModule} from 'primeng/dialog';
+import {GMapModule} from 'primeng/gmap';
+import {BreadcrumbModule} from 'primeng/breadcrumb';
+import {ButtonModule} from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +32,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CardModule,
+    ChartModule,
+    MenuModule,
+    ToolbarModule,
+    AccordionModule,
+    CarouselModule,
+    DialogModule,
+    GMapModule,
+    BreadcrumbModule,
+    ButtonModule,
+    MenubarModule
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
